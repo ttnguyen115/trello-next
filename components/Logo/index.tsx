123adf22344238c75d62d14899ea5f829312301f
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-12-08 20:55:18 +0700

    fix navbar ui and add siteConfig

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -9,9 +9,7 @@ function Logo() {
     <Link href={AppRoutes.HOME_PAGE}>
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
         <Image src="/logo.svg" alt="Logo" height={30} width={30} />
-        <p
-          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
-        >
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           Taskify
         </p>
       </div>
